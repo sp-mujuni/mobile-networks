76eaf7a0d68de86b1ebbd29b7fe69173a6a4347f
smujuni99@gmail.com <smujuni99@gmail.com>
2024-09-19 15:39:02 +0300

    Add code for saving contact information with optional parameters in Dart

--- learn_dart/9.function_access.dart
+++ learn_dart/9.function_access.dart
@@ -4,19 +4,29 @@ String name = 'Alice';
 void main(List<String> args) {
   f1();
   f2();
+  save_contact(name: "John", phone: "1234567890", email: "g.com", address: "123, Street, City");
 }
 
 save_contact({
   required String name,
   String ? phone, // Optional parameter: ? makes it nullable; not required
-  required String email,
+  String ? email,
   String ? address,
 }) {
+  // Save the contact details
+  Map<String, dynamic> data = {};
+  data['name'] = name;
+  if (phone != null) {
+    data['phone'] = "No phone number";
+  }
+  // Terinary operator
+  data['email'] = email == null ? "No email" : email;
+
   print('Name: $name');
   print('Phone: $phone');
   print('Email: $email');
   print('Address: $address');
-})
+}
 
 f1(){
   // String name = 'Bob';
