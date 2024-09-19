0f45da14c6499f9a2f7086cb91446a7a2cbb4dc8
smujuni99@gmail.com <smujuni99@gmail.com>
2024-09-19 15:28:19 +0300

    Add code for saving contact information in Dart

--- learn_dart/9.function_access.dart
+++ learn_dart/9.function_access.dart
@@ -6,6 +6,18 @@ void main(List<String> args) {
   f2();
 }
 
+save_contact({
+  required String name,
+  String ? phone, // Optional parameter: ? makes it nullable; not required
+  required String email,
+  String ? address,
+}) {
+  print('Name: $name');
+  print('Phone: $phone');
+  print('Email: $email');
+  print('Address: $address');
+})
+
 f1(){
   // String name = 'Bob';
   print("Hello $name from f1");
